eb3009a3b46cadafc0a71717b5f98b28a5927245
gosienna <gosienna@gmail.com>
2022-04-08 01:23:13 +0800

    add marker, capture using touch

--- main.js
+++ main.js
@@ -84,9 +84,10 @@ captured_c.addEventListener('touchmove', function(event){
     y = Math.round(event.touches[0].clientY - cRect.top)
     
     captured_ctx.putImageData(captured_imgdata,0,0);
-    captured_ctx.moveTo(x,y);
-    captured_ctx.lineTo(x+10+2,y+10-2);
-    captured_ctx.lineTo(x+10-2,y+10+2);
+    // captured_ctx.moveTo(x,y);
+    // captured_ctx.lineTo(x+10+2,y+10-2);
+    // captured_ctx.lineTo(x+10-2,y+10+2);
+    captured_ctx.arc(x, y, 10, 0, Math.PI*2),
     captured_ctx.fillStyle = "#77f022";
     captured_ctx.fill();
 });
