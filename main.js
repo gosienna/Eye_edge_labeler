cec1fd66754f3afd962a2c1db32fea154ac21afa
gosienna <gosienna@gmail.com>
2022-04-09 00:29:46 +0800

    add mask, add side bar, add drawing utility

--- main.js
+++ main.js
@@ -1,17 +1,23 @@
 
 
 //setup common variable
+let file_name;
 let width_capture_window = 200;
 let height_capture_window = 200;
 let raw_imgdata;
 let captured_imgdata;
+let mask_imgdata;
 let width_original_img;
 let height_original_img;
 let width_img_display = 800; //set fixed width for display
 let height_img_display; //modified height for display
 let captured = false; //flap to indicate wheather image had been selected
+let c = document.getElementById("raw_img")
+let ctx = c.getContext('2d');
 let captured_c = document.getElementById("captured_img")
 let captured_ctx = captured_c.getContext('2d');
+let mask_c= document.getElementById("edge_mask");
+let mask_ctx = mask_c.getContext('2d');
 
 
 //triger one: load sample eye image
@@ -28,6 +34,7 @@ raw_img.onload = function(){
     c.height = height_img_display;
     ctx.drawImage(this,0,0,width_original_img,height_original_img,0,0,width_img_display,height_img_display);
     raw_imgdata=ctx.getImageData(0,0,c.width,c.height);
+
    
   }
 
@@ -35,26 +42,23 @@ raw_img.onload = function(){
 
 //triger two: update image when user upload their own image
 let inputElement=document.getElementById('load_img')
-inputElement.onchange = function() {
+inputElement.onchange = function(event) {
                         //get file Filenam
                         processSelectedFiles(this)
                         //reload the img_original
-                        img_original.src = URL.createObjectURL(event.target.files[0]);
+                        raw_img.src = URL.createObjectURL(event.target.files[0]);
                         }
 
 //function to get file name
 function processSelectedFiles(fileInput) {
-var files = fileInput.files;
+let files = fileInput.files;
 file_name=files[0].name;
 
 }
 
-//set up eventlistener to set up capture window size
-let c = document.getElementById("raw_img")
+//capture eye: touch screen
 c.addEventListener('touchmove', function(event){
     if(captured == false){ //check wheather captured window has been clicked
-        let c = document.getElementById("raw_img")
-        let ctx = c.getContext('2d');
         let cRect = c.getBoundingClientRect()
         x = Math.round(event.touches[0].clientX - cRect.left)
         y = Math.round(event.touches[0].clientY - cRect.top)
@@ -69,29 +73,121 @@ c.addEventListener('touchmove', function(event){
     }
 });
 
+//capture eye: mouse move
+c.addEventListener('mousemove', function(event){
+    if(captured == false){ //check wheather captured window has been clicked
+        let cRect = c.getBoundingClientRect()
+        x = Math.round(event.clientX - cRect.left)
+        y = Math.round(event.clientY - cRect.top)
+        ctx.putImageData(raw_imgdata,0,0);
+        ctx.beginPath();
+        ctx.rect(x-width_capture_window/2, y-height_capture_window/2, width_capture_window, height_capture_window);
+        ctx.strokeStyle = "#77f022";
+        ctx.stroke();
+
+        let s = width_original_img / width_img_display; //ratio between
+        captured_ctx.drawImage(raw_img,x*s-s*width_capture_window/2,y*s-s*height_capture_window/2,s*width_capture_window,s*height_capture_window,0,0,512,512);
+    }
+});
+
+
 c.addEventListener('click', function(event){
     captured = true;
     captured_imgdata = captured_ctx.getImageData(0,0,captured_c.width,captured_c.height);
 });
+let old_x;
+let old_y;
 
-//set up eventlistener to capture touch screen location
+//fill the edge mask with black background
+mask_ctx.fillStyle =  "black";
+mask_ctx.fillRect(0, 0, 512, 512);
+mask_imgdata = mask_ctx.getImageData(0,0,mask_c.width,mask_c.height);
 
-captured_ctx.fillStyle = "#77f022";
+captured_c.addEventListener("touchstart",function(event){
+    let cRect = captured_c.getBoundingClientRect()
+    old_x = Math.round(event.touches[0].clientX - cRect.left)
+    old_y = Math.round(event.touches[0].clientY - cRect.top)
+})
+
+//draw edge: touch screen
 captured_c.addEventListener('touchmove', function(event){
 
     let cRect = captured_c.getBoundingClientRect()
     x = Math.round(event.touches[0].clientX - cRect.left)
     y = Math.round(event.touches[0].clientY - cRect.top)
-    
+    //draw on captured image
     captured_ctx.putImageData(captured_imgdata,0,0);
     captured_ctx.beginPath();
-    captured_ctx.moveTo(x,y);
-    captured_ctx.lineTo(x+10+2,y+10-2);
-    captured_ctx.lineTo(x+10-2,y+10+2);
-    captured_ctx.closePath()
-    //captured_ctx.arc(x, y, 10, 0, Math.PI*2),
-    captured_ctx.fill();
+    captured_ctx.moveTo(old_x , old_y);
+    captured_ctx.lineTo(x,y);
+    captured_ctx.closePath();
+    captured_ctx.strokeStyle = "#77f022";
+    captured_ctx.stroke();
+    captured_imgdata = captured_ctx.getImageData(0,0,captured_c.width,captured_c.height);//update image
+
+    //draw on edge mask
+    //mask_ctx.putImageData(mask_imgdata,0,0);
+    mask_ctx.beginPath();
+    mask_ctx.moveTo(old_x , old_y);
+    mask_ctx.lineTo(x,y);
+    mask_ctx.closePath();
+    mask_ctx.strokeStyle = "white";
+    mask_ctx.stroke();
+    mask_imgdata = mask_ctx.getImageData(0,0,mask_c.width,mask_c.height);//update image
+    
+    //update point location
+    old_x = x;
+    old_y = y;
 });
+
+
+//draw edge: mouse down and move
+
+captured_c.addEventListener("mousedown",function(event){
+    let cRect = captured_c.getBoundingClientRect()
+    old_x = Math.round(event.clientX - cRect.left)
+    old_y = Math.round(event.clientY - cRect.top)
+})
+
+
+function mouseMoveFunction(event) {
+    let cRect = captured_c.getBoundingClientRect()
+    x = Math.round(event.clientX - cRect.left)
+    y = Math.round(event.clientY - cRect.top)
+    //draw on captured image
+    captured_ctx.putImageData(captured_imgdata,0,0);
+    captured_ctx.beginPath();
+    captured_ctx.moveTo(old_x , old_y);
+    captured_ctx.lineTo(x,y);
+    captured_ctx.closePath();
+    captured_ctx.strokeStyle = "#77f022";
+    captured_ctx.stroke();
+    captured_imgdata = captured_ctx.getImageData(0,0,captured_c.width,captured_c.height);//update image
+
+    //draw on edge mask
+    //mask_ctx.putImageData(mask_imgdata,0,0);
+    mask_ctx.beginPath();
+    mask_ctx.moveTo(old_x , old_y);
+    mask_ctx.lineTo(x,y);
+    mask_ctx.closePath();
+    mask_ctx.strokeStyle = "white";
+    mask_ctx.stroke();
+    mask_imgdata = mask_ctx.getImageData(0,0,mask_c.width,mask_c.height);//update image
+    
+    //update point location
+    old_x = x;
+    old_y = y;
+}
+
+captured_c.addEventListener("mousedown", function(e){ 
+    this.addEventListener("mousemove", mouseMoveFunction);
+});
+
+captured_c.addEventListener("mouseup", function(e){
+    this.removeEventListener("mousemove", mouseMoveFunction);
+});
+
+
 
 
 //other functions
@@ -107,7 +203,25 @@ function window_decrease(){
 
 function redo_capture(){
     captured = false;
+    //clear edge mask
+    mask_ctx.fillStyle =  "black";
+    mask_ctx.fillRect(0, 0, 512, 512);
+    mask_imgdata = mask_ctx.getImageData(0,0,mask_c.width,mask_c.height);
+
 }
+
+function save(){
+    let link = document.createElement('a');
+    //download mask
+    link.download = file_name+"_mask.png";
+    link.href = mask_c.toDataURL()
+    link.click();
+    //download captured eye
+    link.download = file_name+"_captured.png";
+    link.href = captured_c.toDataURL()
+    link.click();
+    link.delete;
+  }
 
 
 //prevent the scrolling effect of touching 
